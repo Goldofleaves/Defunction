d4c561fc780677526506eed20776a7b81b5cdfbc
GoldOfLeaves <goldenleaf942@gmail.com>
2026-01-27 23:52:09 +0800

    Update english.lua

--- Localization/english.lua
+++ Localization/english.lua
@@ -1,10 +1,10 @@
 return {
     labels = {
         death = {
-            "|o:black||c:red|ERROR|c:reset|: Player.lua:?: Attempt to index 'Arna' (a nil value)",
+            "|o:black||c:red|ERROR|c:reset|: Player.lua:|t:randomized|??|t:reset|: Attempt to index 'Arna' (a nil value)",
             "|o:black|Stack Traceback:",
-            "|o:black||c:grey|[\"Player.lua\":?]: in function |c:yellow|deathTracker|c:reset|()|c:grey|:",
-            "|o:black||c:grey|[\"Player.lua\":?]: |c:red|DEATHS: {1}",
+            "|o:black||c:grey|[\"Player.lua\":|t:randomized|??|t:reset|]: in function |c:yellow|deathTracker|c:reset|()|c:grey|:",
+            "|o:black||c:grey|[\"Player.lua\":|t:randomized|??|t:reset|]: |c:red|DEATHS: {1}",
             "|o:black||c:blue|Rebooting.."
         }
     }
